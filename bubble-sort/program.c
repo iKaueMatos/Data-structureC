41179ee2fc26b0e80a31d1b920452390e2ea5c16
kaue de matos <kaue178hotimail@gmail.com>
2023-08-30 08:51:34 -0300

    first commit bubble sort algorithm

---
+++ bubble-sort/program.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+#define TAM 10
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+    int numeros[TAM];
+    int i, auxiliar, contador;
+
+    printf("Entre com dez números para preecher o array, e pressione enter após digitar cada um:\n");
+    for (i = 0; i < TAM; i++) {
+        scanf("%d", &numeros[i]);
+    }
+
+    printf("Ordem atual dos itens no array:\n");
+
+
+    for (i = 0; i < TAM; i++) {
+        printf("%4d", numeros[i]);
+    }
+
+    for (contador = 1; contador < TAM; contador++) {
+        for (i = 0; i < TAM - 1; i++) {
+            if (numeros[i] > numeros[i + 1]) {
+                auxiliar = numeros[i];
+                numeros[i] = numeros[i + 1];
+                numeros[i + 1] = auxiliar;
+            }
+        }
+    }
+
+    printf("Elementos do array em ordem crescente: \n");    
+
+    for (i = 0; i < TAM; i++) {
+        printf("%4d", numeros[i]);
+    }
+
+    return 0;
+}
